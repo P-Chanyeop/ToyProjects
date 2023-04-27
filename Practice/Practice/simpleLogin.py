9539d3daf3d10bb1d1487cf593a2477f20df7914
P-Chanyeop <qkrcksduq1gh@naver.com>
2023-04-27 11:11:31 +0900

    add : python examples

--- Practice/Practice/simpleLogin.py
+++ Practice/Practice/simpleLogin.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Practice/simpleLogin.py
 # auth 모듈로 애플리케이션 입력 기능 넣기
 import auth
 
@@ -6,3 +7,16 @@ if auth.login(input_id):
     print('Hello, ' + input_id)
 else:
     print('Who are you?')
+=======
+# 애플리케이션 입력 기능 넣기
+input_id = input("아이디를 입력해주세요.\n")
+# real_egoing = "egoing"
+# real_yeop = "yeop"
+names = ['egoing', 'yeop']
+for name in names:
+    if(name == input_id):
+        print("Hello! " + name)
+        import sys
+        sys.exit()      # 파이썬 시스템 종료. 
+print("Who are you?")        
+>>>>>>> 4295ed5 (create new simple examples by python):Python/simpleLogin.py
